2a1fc73276ee6104a6fd92f4a3d0ef782371f85b
Martin Neff <martin.neff@approppo.ch>
2018-12-07 10:17:04 +0100

    Take Foto

--- app/src/main/java/ch/approppo/memory/ProfileFragment.kt
+++ app/src/main/java/ch/approppo/memory/ProfileFragment.kt
@@ -1,7 +1,12 @@
 package ch.approppo.memory
 
+import android.app.Activity
 import android.content.Context
+import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.os.Bundle
+import android.provider.MediaStore
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.Menu
@@ -9,6 +14,9 @@ import android.view.MenuInflater
 import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ImageView
+import java.io.File
 
 class ProfileFragment : Fragment() {
 
@@ -18,15 +26,28 @@ class ProfileFragment : Fragment() {
 
     private lateinit var callback: OnboardingFlowCallback
 
+    private lateinit var btTakeFoto: Button
+    private lateinit var ivImage: ImageView
+
     override fun onAttach(context: Context?) {
         super.onAttach(context)
-        callback = context as? OnboardingFlowCallback ?:
-                throw IllegalStateException("Host must implement OnboardingFlowCallback")
+        callback = context as? OnboardingFlowCallback ?: throw IllegalStateException("Host must implement OnboardingFlowCallback")
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         setHasOptionsMenu(true)
-        return inflater.inflate(R.layout.fragment_profile, container, false)
+        val view = inflater.inflate(R.layout.fragment_profile, container, false)
+        btTakeFoto = view.findViewById(R.id.bt_take_foto)
+        btTakeFoto.setOnClickListener { takeFoto() }
+        ivImage = view.findViewById(R.id.iv_profile_image)
+        val file = File(requireContext().filesDir, "profile_image.png")
+        if (file.exists()) {
+            file.inputStream().use {
+                val bitmap = BitmapFactory.decodeStream(it)
+                ivImage.setImageBitmap(bitmap)
+            }
+        }
+        return view
     }
 
     override fun onCreateOptionsMenu(menu: Menu?, inflater: MenuInflater?) {
@@ -39,5 +60,23 @@ class ProfileFragment : Fragment() {
             return true
         }
         return super.onOptionsItemSelected(item)
+    }
+
+    private fun takeFoto() {
+        val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        startActivityForResult(intent, 1)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (requestCode == 1 && resultCode == Activity.RESULT_OK) {
+            val image = data?.extras?.get("data") as? Bitmap
+            image?.let { bitmap ->
+                ivImage.setImageBitmap(bitmap)
+                val file = File(requireContext().filesDir, "profile_image.png")
+                file.outputStream().use { os ->
+                    bitmap.compress(Bitmap.CompressFormat.PNG, 100, os)
+                }
+            }
+        }
     }
 }
